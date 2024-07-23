bac6cdcdb3a77d1de38c0b6ffe76110ce98c9952
X <sabinarai@x.broadband>
2024-07-23 15:19:49 +0100

    feat: copy pasted

--- src/main/java/Vehicles/Car.java
+++ src/main/java/Vehicles/Car.java
@@ -5,4 +5,21 @@ public class Car extends Vehicles{
         super(make, model);
         engine = new CarEngine(1,"2");
     }
+
+    @Override
+    protected void accelerate() {
+        String message = make + " " + model + " speed: ";
+        double fuelMod = switch (this.engine.fuelType) {
+            case "Unleaded" -> 1.5D;
+            case "Leaded" -> 1.2D;
+            case "Diesel" -> 1.8D;
+            default -> throw new IllegalStateException("Unexpected value: " + this.engine.fuelType);
+        };
+        for (int i = 0 ; i < 10 ; i++) {
+            System.out.println(message + String.format("%.2f", (engine.horsepower * fuelMod) * Math.exp(Math.sqrt(i)) / 2));
+        }
+
+    }
+
+
 }
