3211741c997280360dd0319a57f0a9cc560ea319
X <sabinarai@x.broadband>
2024-07-23 16:32:21 +0100

    feat: cube private

--- src/main/java/shapes/Cube.java
+++ src/main/java/shapes/Cube.java
@@ -2,9 +2,14 @@ package shapes;
 
 public class Cube extends Shape3D{
     int size;
+    Rectangle base;
 
     public Cube(int size) {
         this.size = size;
+    }
+
+    private Cube(Rectangle base){
+        this.base = base;
     }
 
     @Override
